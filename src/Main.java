cfe8dc00010106f4d268e500ae5406bf9547582c
Bemibam <bemnethailu69@gmail.com>
2025-04-10 09:52:23 -0700

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void go(int age){
+        if(age >= 18){
+            System.out.println("GO VOTE!!!");
+        }else{
+            System.out.println("GO PLAY");
         }
+    }
+
+    public static void respond(char answer){
+        if(answer == 'Y' || answer == 'y'){
+            System.out.println("YUP YUP YUP");
+        }else{
+            System.out.println("NOPE NOPE NOPE");
+        }
+    }
+
+    public static void drink(int age) {
+        if(age < 21){
+            System.out.println("juice");
+        }else if(age < 50) {
+            System.out.println("adult beverage");
+        }else if(age < 77) {
+            System.out.println("tea");
+        }else{
+            System.out.println("heavy beverage");
+        }
+    }
+    public static void thankYou() {
+        System.out.println("Thank you for coming to our restaurant!");
+    }
+
+    public static void main(String[] args) {
+        drink(5); // juice
+        drink(45); // adult beverage
+        drink(51); // tea
+        drink(85); // heavy beverage
+
+        for(String a : args) {
+            System.out.println(a);
+        }
+
+        go(13);
+        go(21);
+
+        respond('Y');
+        respond('N');
+        thankYou();
+
     }
 }
